0cff815483d206ba824a4176fc9a8254fbcab968
Irbe Krumina <irbekrm@gmail.com>
2020-12-13 10:48:31 +0000

    Day four, part 2

--- days/four.go
+++ days/four.go
@@ -5,11 +5,10 @@ import (
 	"io/ioutil"
 	"log"
 	"regexp"
+	"strconv"
 )
 
 const passportFieldRegexp = `([a-z]{3}):([^\s\n]+)`
-
-var requiredFields = 7
 
 func Four() {
 	b, err := ioutil.ReadFile("data/day4.txt")
@@ -28,32 +27,113 @@ func Four() {
 }
 
 type passport struct {
-	byr, iyr, eyr, hgt, hcl, ecl, pid, cid string
+	byr, iyr, eyr, hgt, hcl, ecl, pid, cid passportField
+}
+
+type passportField interface {
+	isValid() bool
+}
+
+type byr string
+
+func (b byr) isValid() bool {
+	i, err := strconv.Atoi(string(b))
+	if err != nil {
+		return false
+	}
+	return i >= 1920 && i <= 2002
+}
+
+type iyr string
+
+func (i iyr) isValid() bool {
+	num, err := strconv.Atoi(string(i))
+	if err != nil {
+		return false
+	}
+	return num >= 2010 && num <= 2020
+}
+
+type eyr string
+
+func (e eyr) isValid() bool {
+	num, err := strconv.Atoi(string(e))
+	if err != nil {
+		return false
+	}
+	return num >= 2020 && num <= 2030
+}
+
+type hgt string
+
+func (h hgt) isValid() bool {
+	re := regexp.MustCompile(`^(\d+)(cm|in)$`)
+	if !re.MatchString(string(h)) {
+		return false
+	}
+	match := re.FindAllStringSubmatch(string(h), -1)
+	num, err := strconv.Atoi(match[0][1])
+	if err != nil {
+		return false
+	}
+	if match[0][2] == "cm" {
+		return num >= 150 && num <= 193
+	}
+	if match[0][2] == "in" {
+		return num >= 59 && num <= 76
+	}
+	return false
+}
+
+type hcl string
+
+func (h hcl) isValid() bool {
+	re := regexp.MustCompile(`^#[0-9a-z]{6}`)
+	return re.MatchString(string(h))
+}
+
+type ecl string
+
+func (e ecl) isValid() bool {
+	re := regexp.MustCompile(`^(amb|blu|brn|gry|grn|hzl|oth)$`)
+	return re.MatchString(string(e))
+}
+
+type pid string
+
+func (p pid) isValid() bool {
+	re := regexp.MustCompile(`^\d{9}$`)
+	return re.MatchString(string(p))
+}
+
+func (p *passport) hasCompulsoryFields() bool {
+	valid := p.byr != nil && p.iyr != nil && p.eyr != nil && p.hgt != nil && p.ecl != nil && p.pid != nil && p.hcl != nil
+	return valid
 }
 
 func (p *passport) isValid() bool {
-	valid := p.byr != "" && p.iyr != "" && p.eyr != "" && p.hgt != "" && p.ecl != "" && p.pid != "" && p.hcl != ""
-	return valid
+	if !p.hasCompulsoryFields() {
+		return false
+	}
+	return p.byr.isValid() && p.iyr.isValid() && p.eyr.isValid() && p.hgt.isValid() && p.ecl.isValid() && p.pid.isValid() && p.hcl.isValid()
 }
 
 func (p *passport) setField(key, value string) {
 	switch key {
 	case "byr":
-		p.byr = value
+		p.byr = byr(value)
 	case "iyr":
-		p.iyr = value
+		p.iyr = iyr(value)
 	case "eyr":
-		p.eyr = value
+		p.eyr = eyr(value)
 	case "hgt":
-		p.hgt = value
+		p.hgt = hgt(value)
 	case "ecl":
-		p.ecl = value
+		p.ecl = ecl(value)
 	case "pid":
-		p.pid = value
-	case "cid":
-		p.cid = value
+		p.pid = pid(value)
 	case "hcl":
-		p.hcl = value
+		p.hcl = hcl(value)
 	}
 }
 
