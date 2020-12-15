d66bcfc155ac255121bbf1239502aa312e8c539b
Irbe Krumina <irbekrm@gmail.com>
2020-12-15 08:36:02 +0000

    Day 7, part 2

--- days/seven.go
+++ days/seven.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 	"log"
 	"regexp"
+	"strconv"
 )
 
 const (
-	bagsRegexp    = `^([a-z]+\s[a-z]+)|\d+\s([a-z]+\s[a-z]+)`
-	shinyGoldBags = "shiny gold"
+	bagsRegexp       = `^([a-z]+\s[a-z]+)|\d+\s([a-z]+\s[a-z]+)`
+	bagsParentRegexp = `^[a-z]+\s[a-z]+`
+	bagsChildRegexp  = `(\d+)\s([a-z]+\s[a-z]+)\sbag`
+	shinyGoldBags    = "shiny gold"
 )
 
 var closure func([]string)
@@ -20,9 +23,33 @@ func Seven() {
 		log.Fatalf("error reading input data: %v", err)
 	}
 	s := split(b, "\n")
-	bags := mapParents(s)
-	sum := countShinyGoldBags(bags)
+	//bags := mapParents(s)
+	//sum := countShinyGoldBags(bags)
+	//fmt.Printf("sum: %v\n", sum)
+	children := mapChildren(s)
+	//fmt.Printf("%#v\n", children)
+	sum := inShinyGold(children)
 	fmt.Printf("sum: %v\n", sum)
+}
+func inShinyGold(allBags map[string][][]interface{}) int {
+	var sum int
+	ch := allBags[shinyGoldBags]
+	for _, child := range ch {
+		sum += countChildren(child[1].(int), child, allBags)
+	}
+	return sum
+}
+
+func countChildren(count int, children []interface{}, allBags map[string][][]interface{}) int {
+	var sum int
+	ch := allBags[children[0].(string)]
+	for _, child := range ch {
+		sum += countChildren(count*child[1].(int), child, allBags)
+	}
+	if len(ch) == 0 {
+		return count
+	}
+	return sum + count
 }
 
 func countShinyGoldBags(bags map[string][]string) int {
@@ -47,6 +74,26 @@ func mapParents(s []string) map[string][]string {
 		parent := match[0][1]
 		for i := 1; i < len(match); i++ {
 			bags[match[i][2]] = append(bags[match[i][2]], parent)
+		}
+	}
+	return bags
+}
+
+func mapChildren(s []string) map[string][][]interface{} {
+	bags := make(map[string][][]interface{}, len(s))
+	parentRe := regexp.MustCompile(bagsParentRegexp)
+	childRe := regexp.MustCompile(bagsChildRegexp)
+	for _, val := range s {
+		parentMatch := parentRe.FindAllString(val, -1)
+		childMatch := childRe.FindAllStringSubmatch(val, -1)
+		parent := parentMatch[0]
+		for _, child := range childMatch {
+			count, err := strconv.Atoi(child[1])
+			if err != nil {
+				log.Fatalf("cannot convert %s to int: %v", child[1], err)
+			}
+			children := []interface{}{child[2], count}
+			bags[parent] = append(bags[parent], children)
 		}
 	}
 	return bags
